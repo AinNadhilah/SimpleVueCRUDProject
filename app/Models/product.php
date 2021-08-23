592bf7d0941088c6800d92bbd2f7760e9eddeff5
Ain Nadel <ain.nadhilah@gmail.com>
2021-08-23 10:54:37 +0800

    reupload

--- app/Models/product.php
+++ app/Models/product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     use HasFactory;
-    protected $fillable = [
+   protected $fillable = [
         'name', 
         'detail'
     ];
